ba80c2b97c049e1a0a1c91406a5838eb9da03ab7
jason <jhchoi0709@gmail.com>
2021-08-01 01:57:06 -0700

    add parse_function that supports default keyword inputs and check necessary keyword inputs.

--- io/parse_function_args.m
+++ io/parse_function_args.m
@@ -1,4 +1,5 @@
 function kwargs = parse_function_args(varargin)
+%% kwargs = parse_function_args(varargin{:});
     kwargs = struct();
     for idx = 1:2:length(varargin)
         kwargs.(varargin{idx}) = varargin{idx+1};
